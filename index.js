fd5621071d1787d0bfaa0ebdb8bf148806522ecc
Fabian Mueller <mueller.fabian@gmail.com>
2015-08-05 08:43:12 +0200

    Move render part to react component; allow live-updates via textarea

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 'use strict';
 
-// var CypherInput = require('./src/react_components/cypherInput.jsx');
-var render = require('./lib/render');
+var React = require('react');
+var CypherInput = require('./src/react_components/cypherInput');
 
-render();
+_.each(window.document.getElementsByClassName('js-react-cypherinput'), function(el) {
+  var attributes = el.attributes;
+
+  var initStatement;
+  if ('data-initial-statement' in attributes) {
+    initStatement = attributes['data-initial-statement'].value;
+  }
+
+  React.render(<CypherInput
+    statement={initStatement}
+    />, el);
+
+});
--- src/react_components/cypherInput.jsx
+++ src/react_components/cypherInput.jsx
@@ -1,15 +1,47 @@
 'use strict';
 
 var React = require('react');
+var CypherUtils = require('../../lib/cypherUtils');
+var utils = new CypherUtils();
+var forceLayout = require('../../lib/forceLayout');
 
 module.exports = React.createClass({
   displayName: 'CypherInput',
   propTypes: {
-
+    statement: React.PropTypes.string
+  },
+  componentWillMount: function() {
+    this.setState({
+      statement: this.props.statement.trim()
+    });
+  },
+  componentDidMount: function() {
+    this.handleUpdate();
+  },
+  handleChange: function(e) {
+    this._update(e.target.value);
+    this.setState({
+      statement: e.target.value
+    });
+  },
+  handleUpdate: function(e) {
+    if (e) { e.preventDefault(); }
+    var imageContainer = this.refs.graphDrawArea.getDOMNode();
+    imageContainer.innerHTML = '';
+    var graph = utils.parseCreate(this.state.statement, {measure: true});
+    forceLayout(graph, imageContainer, {width: 500, height: 500});
   },
   render: function() {
     return (
-      <input type="text"></input>
+      <div className="graph-component">
+        <textarea
+          onChange={this.handleChange}
+          placeholder="Your cypher expression ..."
+          value={this.state.statement}
+          ></textarea>
+        <button onClick={this.handleUpdate}>Update</button>
+        <div className="graph-drawarea" ref="graphDrawArea"></div>
+      </div>
     );
   }
 });
